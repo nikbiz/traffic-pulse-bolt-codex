5496daf22839af9775a4e1d4f629995cfa489536
nikbiz <nikbiz@users.noreply.github.com>
2025-07-29 00:26:45 +0000

    deploy: 431afcf465239e1ef4d6bebc673f251e34f5e676

---
+++ _next/static/chunks/app/services/page-f85b9177a05c3464.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[469,301,104,618,248,309],{9396:function(n,e,u){Promise.resolve().then(u.t.bind(u,4724,23))}},function(n){n.O(0,[724,971,864,744],function(){return n(n.s=9396)}),_N_E=n.O()}]);
